4c2a4b1b617e0827cae70648042a6903b3d38810
sxl2333 <sxl2103322494@qq.com>
2018-08-23 11:51:12 +0800

    头部效果

--- src/App.vue
+++ src/App.vue
@@ -118,10 +118,31 @@
 <script>
 export default {
   name: 'container',
-}
+};
+import $ from "jquery"
+$(document).ready(function() {
+	$("#menu2 li a").wrapInner( '<span class="out"></span>' );
+	$("#menu2 li a").each(function() {
+		$( '<span class="over">' +  $(this).text() + '</span>' ).appendTo( this );
+	});
+
+	$("#menu2 li a").hover(function() {
+		$(".out",	this).stop().animate({'top':	'48px'},	300); // move down - hide
+		$(".over",	this).stop().animate({'top':	'0px'},		300); // move down - show
+
+	}, function() {
+		$(".out",	this).stop().animate({'top':	'0px'},		300); // move up - show
+		$(".over",	this).stop().animate({'top':	'-48px'},	300); // move up - hide
+	});
+
+});
 </script>
 
 <style>
 /*引入样式表*/
 @import url("./assets/statics/site/css/style.css");
+@import url("./assets/lib/css/style.css");
+#menu2{
+    background-image: none;
+}
 </style>
